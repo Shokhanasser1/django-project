a4dbcc732a8ff42b98aee7199670f3830e54eac1
Shokhanasser <shokhanasserprojr@gmail.com>
2024-06-02 21:32:55 +0500

    fixing migrations and creating participants with comments

--- base/models.py
+++ base/models.py
@@ -15,13 +15,14 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants =
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     update = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
     
     class Meta:
         ordering = ['-update', '-created']
+        
     def __str__(self) -> str:
         return self.name
     
