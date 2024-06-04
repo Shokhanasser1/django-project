c991cf9d34275488def3075f8b152e7416339d3c
Shokhanasser <shokhanasserprojr@gmail.com>
2024-06-04 06:00:08 +0500

    login/register page edit profile page

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 # rooms = [
 #     {'id': 1, 'name': 'Lets learn python!'},
 #     {'id': 2, 'name': 'Design with me!'},
@@ -171,3 +171,15 @@ def deleteMessage(request, pk):
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': message})
 
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=request.user)
+    
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user.profile', pk=user.id)
+    
+    return render(request, 'base/update-user.html', {'form': form})
